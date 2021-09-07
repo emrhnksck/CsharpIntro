07cc621eedbd1cb202697862181c99908e25553d
emrehan <emrehan.kisacikk@hotmail.com>
2021-09-08 01:04:22 +0300

    engin demirog c#kamp dersleri

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint
     // class referans tip
-    public interface IEntityRepository<T> where T:class
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> getAll(Expression<Func<T,bool>> filter=null);
 
